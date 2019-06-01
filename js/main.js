ca6f76cec32360c6caeae6d2ca9e3a4802f5db14
Andrew Aleynikov <drew.lake@yandex.ru>
2019-06-01 13:35:51 +0300

    Update css and js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function showLinks() {
 
-    $('.links-group').css({ marginTop: 200 }).fadeIn({ queue: false, duration: 2000 });
+    $('.links-group').css({ marginTop: 100 }).fadeIn({ queue: false, duration: 2000 });
     $('.links-group').animate({
         marginTop: 0
     }, 1500);
