bff01be7971d37dc1d168079a9cbaf7193503e07
Andrew Aleynikov <drew.lake@yandex.ru>
2019-06-02 17:39:41 +0300

    more smoothness, small css, js rebuild

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 function showLinks() {
-
+    $('.dot').fadeIn({ queue: false, duration: 2000 });
+    $('.sentence').fadeIn({ queue: false, duration: 2000 });
     $('.links-group').css({ marginTop: 100 }).fadeIn({ queue: false, duration: 2000 });
     $('.links-group').animate({
         marginTop: 0
     }, 1500);
-    setTimeout(showDog, 2000);
+    setTimeout(showDog, 1000);
 };
 
-function showDog(){
+function showDog() {
     $(".myDog").fadeIn(2000);
 }
 
-$(".page-body").fadeIn(2000);
+$(document).ready(function () {
+    $(".page-body").fadeIn(2000);
+    setTimeout(showLinks, 2000);
+});
 
-setTimeout(showLinks, 2000);
 
 
